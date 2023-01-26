f682c69b08672731bf04f1e54eefdc46b527df58
Josiah Cook <89039480+Josiah-Cook@users.noreply.github.com>
2023-01-26 10:26:38 -0600

    Update main.js

--- main.js
+++ main.js
@@ -2,6 +2,7 @@
 if(/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)){
     // Hide Items that are not Mobile Friendly
     document.getElementsByClassName('Mobile').style.visibility='hidden';
+    document.getElementById('nav').style.fontSize='30';
   }else{
     // Shows Items that are not Mobile Friendly but are PC fine
     document.getElementsByClassName('Mobile').style.visibility='visible';
