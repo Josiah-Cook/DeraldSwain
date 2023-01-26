c8a53677826565c47806c5baf4258800b78407be
Josiah Cook <89039480+Josiah-Cook@users.noreply.github.com>
2023-01-26 10:32:24 -0600

    Updated Javascript to be more Mobile Friendly

--- main.js
+++ main.js
@@ -3,9 +3,11 @@ if(/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigat
     // Hide Items that are not Mobile Friendly
     document.getElementsByClassName('Mobile').style.visibility='hidden';
     document.getElementById('nav').style.fontSize='30';
+    document.getElementById('logo').style.width='100'
   }else{
     // Shows Items that are not Mobile Friendly but are PC fine
     document.getElementsByClassName('Mobile').style.visibility='visible';
+    document.getElementById('logo').style.width='50'
   }
 
 
