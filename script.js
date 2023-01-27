0532443d958f63d56a417b5cc54a6d6aaac1bffc
Josiah Cook <89039480+Josiah-Cook@users.noreply.github.com>
2023-01-27 10:06:51 -0600

    Fix Javascript problem

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
 import 'styles.css';
 // Determine if Device is a Phone
 if(/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)){
-    // Hide Items that are not Mobile Friendly
-    document.getElementsByClassName('Mobile').style.visibility='hidden';
-    document.getElementById('nav').style.fontSize='50';
-    document.getElementById('logo').style.width='40'
-  }else{
-    // Shows Items that are not Mobile Friendly but are PC fine
-    document.getElementsByClassName('Mobile').style.visibility='visible';
-    document.getElementById('logo').style.width='20'
-      document.getElementById('nav').style.fontSize='30';
-  }
+  // Hide Items that are not Mobile Friendly
+  document.getElementsByClassName('Mobile').style.visibility='hidden';
+  document.getElementById('nav').style.fontSize='50';
+  document.getElementById('logo').style.width='40'
+}else{
+  // Shows Items that are not Mobile Friendly but are PC fine
+  document.getElementsByClassName('Mobile').style.visibility='visible';
+  document.getElementById('logo').style.width='20'
+    document.getElementById('nav').style.fontSize='30';
+}
 
 
