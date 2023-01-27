7dc18a2f3e80f20a7f35f0d6a0e164db88ec3933
Josiah Cook <89039480+Josiah-Cook@users.noreply.github.com>
2023-01-27 09:59:54 -0600

    Connected Javascript and CSS

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+import 'styles.css';
 // Determine if Device is a Phone
 if(/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)){
     // Hide Items that are not Mobile Friendly
